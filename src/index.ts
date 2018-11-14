4af14e126e70fd79fb4db4d1adf128112b9e6d7c
Deivid Teixeira <deividbatfish2@gmail.com>
2018-11-14 13:39:19 -0200

    comenta metodo utilizado para testes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import protractorPg from './Plugin/plugin'
 
-(async ()=> {
+/*(async ()=> {
     await protractorPg.initializer("emissor", "descrição");
     await protractorPg.postTest(true, {name: "teste 1", category: "step 1"})
     await protractorPg.postTest(true, {name: "teste 2", category: "step 1"})
-})()
+})()*/
 
 export = protractorPg;
