97811bbbb2fa6a1a0ca0722fc56f5a86fe1dca0b
Deivid Teixeira <deividbatfish2@gmail.com>
2018-11-14 00:53:25 -0200

    Exporta funcao

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import protractorPg from './Plugin/plugin'
 
-(async ()=> {
+/*(async ()=> {
     await protractorPg.initializer("emissor");
     protractorPg.postTest(true, {name: "teste 22", category: "step 1"})
-})()
+})()*/
 
 export = protractorPg;
