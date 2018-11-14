99349d54fdb2110f196dc91101268e9476df5059
Deivid Teixeira <deividbatfish2@gmail.com>
2018-11-14 15:18:37 -0200

    Refatora criação de cenarios de teste

--- src/Plugin/plugin.ts
+++ src/Plugin/plugin.ts
@@ -1,10 +1,7 @@
 import {ProtractorPlugin} from 'protractor';
-import { ProjetoRepository } from '../repository/projeto.repository';
 import { Projeto } from '../entity/Projeto';
 import { ProjectSingleton } from '../model/project.singletom';
-import { CenarioRepository } from '../repository/cenario.repository';
 import { Cenario } from '../entity/Cenario';
-import { ConfigConnection } from '../connection/configConnection';
 import {Info} from '../model/info'
 import { ProjetoService } from '../service/ProjetoService';
 import { CenarioService } from '../service/CenarioService';
